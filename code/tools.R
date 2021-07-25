ee08f2820d577043aa1bb349e67a5d299c2ed88c
Nicholas D. Socci <soccin@mskcc.org>
2021-07-25 16:09:59 -0400

    Added maf IO

--- code/tools.R
+++ code/tools.R
@@ -1,4 +1,4 @@
-getMAFHeader<-function(fname){
+read_mafHeader<-function(fname){
 
     header=readLines(fname,100)
     colNameRow=grep("^Hugo_Symbol",header)
@@ -9,4 +9,40 @@ getMAFHeader<-function(fname){
 
     return(header[1:(colNameRow-1)])
 
+}
+
+addETAGtoMAF <- function(maf) {
+     mutate(maf,ETAG=paste0(
+        Chromosome,":",
+        Start_Position,"-",End_Position,":",
+        Reference_Allele,":",Tumor_Seq_Allele2
+        )
+    )
+}
+
+.MAF_NUMERIC_COLS=c(
+    "Start_Position", "End_Position",
+    "t_depth", "t_ref_count", "t_alt_count",
+    "n_depth", "n_ref_count", "n_alt_count",
+    "t_var_freq", "n_var_freq",
+    "DISTANCE"
+    )
+
+
+read_maf<-function(fname) {
+    read_tsv(fname,comment="#",col_types = cols(.default = "c")) %>%
+        addETAGtoMAF %>%
+        mutate_at(.MAF_NUMERIC_COLS,as.numeric)
+}
+
+dropEmptyColumns<-function(tbl) {
+    allNACols=names(which(apply(tbl,2,function(x){all(is.na(x))})))
+    select(tbl,-all_of(allNACols))
+}
+
+write_maf<-function(maf,mafFile,mafHeader=NULL) {
+    if(!is.null(mafHeader)) {
+        write(mafHeader,mafFile)
+    }
+    write_tsv(maf,mafFile,na="",append=!is.null(mafHeader),col_names=T)
 }
