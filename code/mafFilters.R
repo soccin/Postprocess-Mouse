a85d4d00e11c4bf0eca8db7f67ec41c1e7ff66a2
Nicholas D. Socci <soccin@mskcc.org>
2021-07-25 16:10:22 -0400

    Moved ETAG func to tools

--- code/mafFilters.R
+++ code/mafFilters.R
@@ -1,8 +1,15 @@
-addETAGtoMAF <- function(maf) {
-     mutate(maf,ETAG=paste0(
-        Chromosome,":",
-        Start_Position,"-",End_Position,":",
-        Reference_Allele,":",Tumor_Seq_Allele2
-        )
-    )
+
+filter.TargettedEvents<-function(maf,targets) {
+
+    maf.events=maf %>% select(chr=Chromosome,start=Start_Position,end=End_Position,ETAG) %>%
+        mutate(start=as.numeric(start),end=as.numeric(end)) %>%
+        distinct
+
+    events_targetted=genome_intersect(maf.events,targets,by = c("chr", "start", "end")) %>% filter(!grepl("^rs\\d+",DAT))
+
+    maf %>%
+        mutate(filter.Targetted=ifelse(ETAG %in% events_targetted$ETAG,"",paste0("NotTargetted::",TARGET_TAG))) %>%
+        mutate(TargetPanel=ifelse(ETAG %in% events_targetted$ETAG,TARGET_TAG,""))
+
 }
+
