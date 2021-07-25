043404e646ab2d16d58a58facb6dcbb4e1595f82
Nicholas D. Socci <soccin@mskcc.org>
2021-07-25 00:03:17 -0400

    Part I of filterTargetted

--- code/filterToTargets.R
+++ code/filterToTargets.R
@@ -1,11 +1,14 @@
-source("~/.Rprofile")
+if(!interactive()) source("~/.Rprofile")
 args=commandArgs(trailing=T)
 if(len(args)<2) {
     cat("\n\tusage: filterToTargets.R TARGETS.BED INPUT.maf [OUTPUT.maf]\n\n")
     quit()
 }
 
-suppressPackageStartupMessages(require(tidyverse))
+suppressPackageStartupMessages({
+    require(tidyverse)
+    require(tidygenomics)
+    })
 
 TARGETS=args[1]
 INPUT_MAFFILE=args[2]
@@ -18,4 +21,31 @@ if(len(args)==3) {
 
 SDIR=Sys.getenv("SDIR")
 
-#source("tools.R")
+source(file.path(SDIR,"tools.R"))
+
+bed4Cols=cols(
+  chr = col_character(),
+  start = col_double(),
+  end = col_double(),
+  DAT = col_character()
+)
+
+targets=read_tsv(TARGETS,col_names=c("chr","start","end","DAT"),col_types=bed4Cols)
+
+mafHeader=getMAFHeader(INPUT_MAFFILE)
+maf=read_tsv(INPUT_MAFFILE,comment="#",col_types = cols(.default = "c")) %>%
+    mutate(ETAG=paste0(
+        Chromosome,":",
+        Start_Position,"-",End_Position,":",
+        Reference_Allele,":",Tumor_Seq_Allele2
+        )
+    )
+
+maf.events=maf %>% select(chr=Chromosome,start=Start_Position,end=End_Position,ETAG) %>%
+    mutate(start=as.numeric(start),end=as.numeric(end)) %>%
+    distinct
+
+
+events_targetted=genome_intersect(maf.events,targets) %>% filter(!grepl("^rs\\d+",DAT))
+
+maf.filt=maf %>% filter(ETAG %in% events_targetted$ETAG)
