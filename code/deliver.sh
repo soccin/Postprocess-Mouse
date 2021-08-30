d076f0631a5be85b00a1b1f5dcceff9b9b7ac1dd
Nicholas D. Socci <soccin@mskcc.org>
2021-08-30 18:01:51 -0400

    Update deliver script

--- code/deliver.sh
+++ code/deliver.sh
@@ -21,20 +21,36 @@ echo
 sudo mkdir -p $PIPELINEDIR/post
 sudo chmod -R g+w $PIPELINEDIR/post
 
-FACETSFILE=$POSTDIR/post/__Facets_PostV5.txt
-if [ -e $FACETSFILE ]; then
-    cp -v $FACETSFILE $PIPELINEDIR/post
-else
-    echo
-    echo "FACETS File does not exists copying SOMATIC file"
-    echo
-    cp -v $POSTDIR/post/*_haplotect_VEP_MAF__PostV5.txt $PIPELINEDIR/post
-fi
+cp -v $POSTDIR/post/*_VEP_MAF__PostV6b.txt $PIPELINEDIR/post
+cp -v $POSTDIR/post/*_VEP_MAF__PostV6b_HQ.xlsx $PIPELINEDIR/post
 
-if [ -e "${PROJECTNO}_haplotect_VEP_MAF__PostV5.xlsx" ]; then
-    echo "Copying XLSX MAF"
-    cp -v ${PROJECTNO}_haplotect_VEP_MAF__PostV5.xlsx $PIPELINEDIR/post
-fi
+cat << EOM
+=====
+Subject: Mouse IMPACT (${PROJECTNO}) results ready
 
-#cp -v $POSTDIR/post/Proj*___FILLOUT.V5.txt $PIPELINEDIR/post
-#./postDeliver.sh $POSTDIR
+The output for Mouse IMPACT ${PROJECTNO} are ready.
+
+You can access them on the BIC Delivery server at:
+
+  https://bicdelivery.mskcc.org/project/${PROJECTNO/Proj_/}/variants/r_001
+
+There is a brief writeup of the various output files is attached below. In addition to the full pipeline output in the above folders attached is an excel file which as filtered mutation list echo to contain just those events that:
+
+    (1) Will change the protein sequence (mis/non-sense, splice-site)
+
+    (2) Are not in the latest version of dbSNP (ie annotated as novel)
+
+    (3) Mutations that were seen in any strain/litter-mate control samples.
+
+    (4) Mutations that have significant counts in a library of control samples
+
+Note in particular filter (2) can miss potential pathogenic events. We are working to clean up dbSNP filtering but if you require more precise control of event filter you can get the full event MAF on the server.
+
+If you require any further assistance let me know. If you are collaborating with a data-analyst in the CMO/Componc we can make the files directly available to them on the MSK cluster.
+
+Nicholas Socci
+Bioinformatics Core
+MSKCC
+socci@cbio.mskcc.org
+EOM
+
