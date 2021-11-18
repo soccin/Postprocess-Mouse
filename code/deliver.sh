13c036ffa7327acbff9bdb38914ea600e4d07a6c
Nicholas D. Socci <soccin@mskcc.org>
2021-11-18 17:45:10 -0500

    Get delivery notes from output description file

--- code/deliver.sh
+++ code/deliver.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+SDIR="$( cd "$( dirname "$0" )" && pwd )"
+
+if [ "" ]; then
+
 POSTDIR=$1
 
 if [ ! -e $POSTDIR/config ]; then
@@ -24,6 +28,10 @@ sudo chmod -R g+w $PIPELINEDIR/post
 cp -v $POSTDIR/post/*_VEP_MAF__PostV6b.txt $PIPELINEDIR/post
 cp -v $POSTDIR/post/*_VEP_MAF__PostV6b_HQ.xlsx $PIPELINEDIR/post
 
+else
+    PROJECTNO=12345_AB
+fi
+
 cat << EOM
 =====
 Subject: Mouse IMPACT (${PROJECTNO}) results ready
@@ -34,18 +42,11 @@ You can access them on the BIC Delivery server at:
 
   https://bicdelivery.mskcc.org/project/${PROJECTNO/Proj_/}/variants/r_001
 
-There is a brief writeup of the various output files is attached below. In addition to the full pipeline output in the above folders attached is an excel file which has filtered mutation list echo to contain just those events that:
+EOM
 
-    (1) Will change the protein sequence (mis/non-sense, splice-site)
+#cat $SDIR/outputDesc.md
 
-    (2) Are not in the latest version of dbSNP (ie annotated as novel)
-
-    (3) Mutations that were seen in any strain/litter-mate control samples.
-
-    (4) Mutations that have significant counts in a library of control samples
-
-Note in particular filter (2) can miss potential pathogenic events. We are working to clean up dbSNP filtering but if you require more precise control of event filter you can get the full event MAF on the server.
-
+cat << EOM
 If you require any further assistance let me know. If you are collaborating with a data-analyst in the CMO/Componc we can make the files directly available to them on the MSK cluster.
 
 Nicholas Socci
